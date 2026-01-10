65d87685f8cc74c07879188e14cd841eb966e474
mr-ahtashamulhaq <mr.ahtashamulhaq@gmail.com>
2026-01-10 18:14:32 +0500

    Create 09_order_by_basics.sql

---
+++ 09_order_by_basics.sql
@@ -0,0 +1,15 @@
+-- Users sorted by age ascending
+SELECT * FROM users
+ORDER BY age;
+
+-- Users sorted by age descending
+SELECT * FROM users
+ORDER BY age DESC;
+
+-- Products sorted by price descending
+SELECT * FROM products
+ORDER BY price DESC;
+
+-- Products sorted by stock quantity ascending
+SELECT * FROM products
+ORDER BY stock_quantity;
