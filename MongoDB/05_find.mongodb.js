180a59a2ad64690fd0869577d5b2786e284463ae
mr-ahtashamulhaq <mr.ahtashamulhaq@gmail.com>
2026-03-08 23:17:42 +0500

    Update 05_find.mongodb.js

--- MongoDB/05_find.mongodb.js
+++ MongoDB/05_find.mongodb.js
@@ -1,3 +1,22 @@
 //We Use find method to get stored documents
 
 // .find({Query}, {Projection})
+
+//Find all documents
+db.test.find()
+
+
+//Find documents with CGPA = 3.2
+db.test.find({CGPA:3.2})
+
+
+//Find documents with age 19
+db.test.find({age:19})
+
+
+//Only Names of all the students presented in Database
+db.test.find({}, {name:true, _id:false}) // -> true mean include and false mean exclude
+
+
+//Only names of students whose CGPA is 3.2
+db.test.find({CGPA:3.2},{name:true, _id:false})
