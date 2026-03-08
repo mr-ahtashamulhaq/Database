787f22086ac7662125c5633bbd042848b31f708a
mr-ahtashamulhaq <mr.ahtashamulhaq@gmail.com>
2026-03-08 22:49:15 +0500

    Create 01-database.mongodb.js

---
+++ MongoDB/01-database.mongodb.js
@@ -0,0 +1,20 @@
+// Show all databases
+show dbs
+
+// Switch to practicedb
+use practicedb
+
+// Create collection
+db.createCollection('users')
+
+// Create schoolDB
+use schooldb
+
+// Create Collection in Student Database
+db.createCollection('students')
+
+// Drop student Database
+db.dropDatabase()
+
+// Use Practise DB
+use practicedb
