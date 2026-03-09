c1076f4179c60c3e74b7874c1e6b6a4f4593ce13
mr-ahtashamulhaq <mr.ahtashamulhaq@gmail.com>
2026-03-09 11:42:30 +0500

    Create 02_collections.mongodb.js

---
+++ MongoDB/02_collections.mongodb.js
@@ -0,0 +1,14 @@
+// show All Collections
+show collections
+
+// Create Collection
+db.createCollection("student")
+
+// Create Collection
+db.createCollection("Teacher")
+
+// Drop Teacher Collection
+db.Teacher.drop()
+
+// Drop student Collection
+db.student.drop()
